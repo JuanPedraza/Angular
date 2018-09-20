6ec7237e37922fefaa4c57c62941715433b3fb76
JuanPedraza <dark2022@gmail.com>
2018-09-19 21:00:27 -0500

    Trabajando con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { test } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    test
   ],
   imports: [
     BrowserModule
@@ -14,3 +16,6 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
